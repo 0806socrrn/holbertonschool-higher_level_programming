11ad082dc1659da35d7aef8e2bad37ed5a004d72
0806socrrn <socrrn@gmail.com>
2022-05-24 18:05:55 -0500

    Classes and Objects

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """creates a class Square"""
-
-
 class Square:
     """clase que define un cuadrado"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Define the class of square"""
 class Square:
+    """square with __size (int) attributes"""
     def __init__(self, size):
+        """square with attributes size (int) that returns nothing"""
         self.__size = size
