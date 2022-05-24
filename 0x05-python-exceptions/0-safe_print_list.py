65e271c8d7faa32a2887409fff101780dbbaaee6
0806socrrn <socrrn@gmail.com>
2022-05-23 19:35:08 -0500

    exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,16 +3,12 @@ from tokenize import Number
 
 
 def safe_print_list(my_list=[], x=0):
-    Number = 0
-    for element in my_list:
-        if Number >= x:
-            break
-        try:
-            print(element, end="")
-        except:
-            break
-        else:
-            Number += 1
-
-    print("")
-    return Number
+    number = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            number += 1
+    except Exception:
+        pass
+    print()
+    return number
