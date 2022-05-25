6b5e801ad7b8a6409841d158662257d792a65ebc
0806socrrn <socrrn@gmail.com>
2022-05-24 21:44:13 -0500

    Classes and Objects

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""define a square based 1-square.py"""
 class Square:
     def __init__(self, size=0):
+        """defines the private distance attribute"""
         if (not isinstance(size, int)):
+            """size whole number"""
             raise TypeError("size must be an integer")
         if (size < 0):
+            """greater than zero"""
             raise TypeError("size must be >= 0")
         self.__size = size
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+"""define a square based 3-square.py"""
 class Square:
+    """class Square"""
     def __init__(self, size=0):
+        """defines the private distance attribute"""
         if (not isinstance(size, int)):
             raise TypeError("size must be an integer")
         if (size < 0):
+            """square with attributes size (int) that returns nothing"""
             raise TypeError("size must be >= 0")
         self.__size = size
     def area(self):
+        """Public instance"""
         return self.__size ** 2
