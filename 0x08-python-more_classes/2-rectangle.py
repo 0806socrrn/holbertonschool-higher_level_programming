223920db40f91bf3e7c9a4e27d55a39f413f86e2
0806socrrn <socrrn@gmail.com>
2022-05-31 21:15:27 -0500

    classes

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,43 +1,51 @@
 #!/usr/bin/python3
+"""
+This module has a simple rectangle class
+"""
+
+
 class Rectangle:
-    """ Rectangle definition """
+    """Basic rectangle with width and height"""
+
     def __init__(self, width=0, height=0):
-        """ Initialize """
+        """Initializes a rectangle with optional height and width"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """Getter for rectangle width"""
         return self.__width
 
     @width.setter
-    def width(self, width):
-        if not isinstance(width, int):
-            raise TypeError('width must be an integer')
-
-        if width < 0:
-            raise ValueError('width must be >= 0')
-
-        self.__width = width
+    def width(self, value):
+        """Setter for rectangle width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
+        """Getter for rectangle height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError('height must be an integer')
-
+        """Setter for rectangle height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError('height must be >= 0')
-
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
+        """Function to get the area of the rectangle"""
         return self.width * self.height
 
     def perimeter(self):
-        if self.width == 0 or self.height == 0:
+        """Function to get the perimeter of the rectangle"""
+        if self.width is 0 or self.height is 0:
             return 0
-        return 2 * (self.width + self.height)
+        return self.width * 2 + self.height * 2
