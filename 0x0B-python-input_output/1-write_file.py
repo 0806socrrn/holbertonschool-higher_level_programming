aa725b609ddb89ee87373215372080d9119e4c81
0806socrrn <socrrn@gmail.com>
2022-06-08 17:32:09 -0500

    Python - Inheritance

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,10 +2,12 @@
 """function that writes a string to a text file (UTF8) 
 and returns the number of characters written"""
 
-
-def write_file(filename="", text=""):
-    """This function writes
-    filename: file to wirte to"""
-
+def number_of_lines(filename=""):
+    """This function returns the number of lines in a given file
+    Args:
+        filename (str): The name of the file to be analyzed
+    Returns:
+        int: The number of lines in the file
+    """
     with open(filename, "r") as f:
         return sum(1 for x in f)
