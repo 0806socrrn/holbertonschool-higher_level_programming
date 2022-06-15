2f9eab5535f5a3492f18b1a71bdfd1a9eb0debdc
0806socrrn <socrrn@gmail.com>
2022-06-14 20:56:41 -0500

    Python - Almost a circle

---
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,13 @@
+import unittest
+from models.base import Base
+
+class TestBase(unittest.TestCase):
+    def test_instantiation(self):
+        b1 = Base()
+        print(b1.id)
+        self.assertEqual(b1.id, 1)
+
+        b2 = Base()
+        print(b2.id)
+        self.assertEqual(b2.id, 2)
+
