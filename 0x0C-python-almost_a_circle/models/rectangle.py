425da9d8ce92f04f8739114c18b0fb1c0c704058
0806socrrn <socrrn@gmail.com>
2022-06-14 22:05:21 -0500

    Python - Almost a circle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""
+Base Rectangle
+"""
+from models.base import Base
+class Rectangle(Base):
+    """
+    Class Rectangle
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initialization
+        """
+        super().__init__(id)
+        self.width=width
+        self.height=height
+        self.x=x
+        self.y=y
+    @property
+    def width(self):
+        """
+        Get width
+        """
+        return self.__width
+    @property
+    def height(self):
+        """
+        Get height
+        """
+        return self.__height
+    @property
+    def x(self):
+        """
+        Get x
+        """
+        return self.__x
+    @property
+    def y(self):
+        """
+        Get y
+        """
+        return self.__y
+
+    @width.setter
+    def width(self, new_data):
+        """
+        Set width
+        """
+        self.__width=new_data
+
+    @height.setter
+    def height(self, new_data):
+        """
+        Set height
+        """
+        self.__height=new_data
+    @x.setter
+    def x(self, new_data):
+        """
+        Set x
+        """
+        self.__x=new_data
+    @y.setter
+    def y(self, new_data):
+        """
+        Set y
+        """
+        self.__y=new_data
