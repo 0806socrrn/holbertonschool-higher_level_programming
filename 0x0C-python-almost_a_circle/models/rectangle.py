93831889e58de3726418fdd9b32f8bb3c09f8282
0806socrrn <socrrn@gmail.com>
2022-06-16 11:52:20 -0500

    Python - Almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -91,3 +91,9 @@ class Rectangle(Base):
         if new_data < 0:
             raise TypeError("y must be >= 0")
         self.__y = new_data
+
+    def area(self):
+        """
+        Area
+        """
+        return(self.width * self.height)
