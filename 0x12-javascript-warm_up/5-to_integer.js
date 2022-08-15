1b53dc397862c7b5b66ecda8fdce6a03b70ecdc8
0806socrrn <socrrn@gmail.com>
2022-08-15 15:24:40 -0500

    🚧 construction my javascript

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,5 @@
-/*Write a script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer:
-
-If the argument can’t be converted to an integer, print “Not a number”
-You must use console.log(...) to print all output
-You are not allowed to use var
-You are not allowed to use try/catch*/
+#!/usr/bin/node
+// Write a script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer
+const argv = require('process').argv;
+const number = parseInt(Number(argv[2]));
+console.log(isNaN(number) ? 'Not a number' : `My number: ${number}`);
