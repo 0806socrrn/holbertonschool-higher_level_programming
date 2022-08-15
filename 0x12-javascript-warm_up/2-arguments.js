556bb8db7373d2ee78dadeb33411798e0d556fe3
0806socrrn <socrrn@gmail.com>
2022-08-15 15:14:54 -0500

    🚧 construction my javascript

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,10 @@
-/*Write a script that prints a message depending of the number of arguments passed:
-If no arguments are passed to the script, print “No argument”
-If only one argument is passed to the script, print “Argument found”
-Otherwise, print “Arguments found”
-You must use console.log(...) to print all output
-You are not allowed to use var*/
+#!/usr/bin/node
+// Write a script that prints a message depending of the number of arguments passed
+const { argv } = require('process');
+if (argv.length < 3) {
+  console.log('No argument');
+} else if (argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
