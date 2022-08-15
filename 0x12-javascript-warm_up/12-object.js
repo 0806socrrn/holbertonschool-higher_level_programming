e0ae3f6aefabe63fb1dfa6c88b936d60f7e950d2
0806socrrn <socrrn@gmail.com>
2022-08-15 16:27:22 -0500

    🚧 construction my javascript

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,3 +1,9 @@
-/*Update this script to replace the value 12 with 89:
-
-You are not allowed to use var*/
+#!/usr/bin/node
+// Update this script to replace the value 12 with 89
+const MyObject = {
+  type: 'object',
+  value: 12
+};
+console.log(MyObject);
+MyObject.value = 89;
+console.log(MyObject);
