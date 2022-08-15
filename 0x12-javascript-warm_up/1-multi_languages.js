2332f7e8ba9c05c19168913de0578f98f1475ac3
0806socrrn <socrrn@gmail.com>
2022-08-15 14:56:29 -0500

    🚧 construction my javascript

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
-/*Write a script that prints 3 lines:
-The first line: “C is fun”
-The second line: “Python is cool”
-The third line: “JavaScript is amazing”
-You must use console.log(...) to print all output
-You are not allowed to use var*/
+#!/usr/bin/node
+// Write a script that prints 3 lines
+const myVarA = 'C is fun\n';
+const myVarB = 'Python is cool\n';
+const myVarC = 'JavaScript is amazing';
+console.log(myVarA + myVarB + myVarC);
