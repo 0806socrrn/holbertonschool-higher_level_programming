27b40a308db2fe55ec7425335b88de1e478582ac
0806socrrn <socrrn@gmail.com>
2022-08-15 16:24:01 -0500

    🚧 construction my javascript

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,9 @@
-/*Write a script that searches the second biggest integer in the list of arguments.
+#!/usr/bin/node
+// Write a script that searches the second biggest integer in the list of arguments.
+const { argv } = require('process');
 
-You can assume all arguments can be converted to integer
-If no argument passed, print 0
-If the number of arguments is 1, print 0
-You must use console.log(...) to print all output
-You are not allowed to use var*/
+const penultimate = argv.slice(2)
+  .sort((a, b) => b - a)
+  .filter((item, index, array) => array.indexOf(item) === index)[1];
+
+console.log(penultimate || 0);
