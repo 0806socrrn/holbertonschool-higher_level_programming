9cef7af292e0402014b64b37ccac7c407bc3e500
0806socrrn <socrrn@gmail.com>
2022-08-16 16:32:08 -0500

    🚧 construction project

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-/*Write a function that prints the number of arguments already printed and the new argument value. (see example below)
+/* Write a function that prints the number of arguments already printed and the new argument value. (see example below)
 Prototype: exports.logMe = function (item)
-Output format: <number arguments already printed>: <current argument value>*/
+Output format: <number arguments already printed>: <current argument value> */
+exports.logMe = ((counter = 0) => function (item) {
+  console.log(`${counter++}: ${item}`);
+})();
