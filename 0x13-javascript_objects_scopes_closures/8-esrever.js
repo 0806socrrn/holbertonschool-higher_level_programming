f4e16633796592457f1570c097fce43de946639e
0806socrrn <socrrn@gmail.com>
2022-08-16 16:31:07 -0500

    🚧 construction project

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,10 +3,11 @@
 Prototype: exports.esrever = function (list)
 You are not allow to use the built-in method reverse */
 exports.esrever = function (list) {
-  const reversedArray = [];
-
-  for (const rev in list) {
-    reversedArray[rev] = list[list.length - rev - 1];
+  if (list.length === 1) {
+    return ([list.pop()]);
   }
-  return (reversedArray);
+  if (list.length === 0) {
+    return ([]);
+  }
+  return [list.pop()].concat(exports.esrever(list));
 };
