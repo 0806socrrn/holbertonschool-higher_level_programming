70dbb5b254b903d23299dba8ddefc870a4ca438e
0806socrrn <socrrn@gmail.com>
2022-08-16 09:59:10 -0500

    🚧 construction my javascript

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,5 +1,8 @@
-/*Write a function that executes x times a function.
-
-The function must be visible from outside
-Prototype: function (x, theFunction)
-You are not allowed to use var*/
+#!/usr/bin/node
+// Write a function that executes x times a function.
+exports.callMeMoby = function (x, theFunction) {
+  let i = 0;
+  for (; i < x; i++) {
+    theFunction();
+  }
+};
