dcb718091d7a0b16fea44ee6795e40354f335764
0806socrrn <socrrn@gmail.com>
2022-08-16 16:18:49 -0500

    🚧 construction project

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -7,17 +7,17 @@ Initialize the instance attribute height with the value of h
 If w or h is equal to 0 or not a positive integer, create an empty object
 Create an instance method called print() that prints the rectangle using the character X */
 class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
+    constructor (w, h) {
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+      }
+    }
+  
+    print () {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
     }
   }
-
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('x'.repeat(this.width));
-    }
-  }
-}
-module.exports = Rectangle;
+  module.exports = Rectangle;
