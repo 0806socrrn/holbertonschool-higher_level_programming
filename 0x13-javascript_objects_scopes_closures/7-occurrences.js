955431a23786421fdfdc0e551423009fb7707c46
0806socrrn <socrrn@gmail.com>
2022-08-16 16:27:30 -0500

    🚧 construction project

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,3 +1,12 @@
 #!/usr/bin/node
-/*Write a function that returns the number of occurrences in a list:
-Prototype: exports.nbOccurences = function (list, searchElement)*/
+/* Write a function that returns the number of occurrences in a list:
+Prototype: exports.nbOccurences = function (list, searchElement) */
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (const search in list) {
+    if (list[search] === searchElement) {
+      count++;
+    }
+  }
+  return (count);
+};
