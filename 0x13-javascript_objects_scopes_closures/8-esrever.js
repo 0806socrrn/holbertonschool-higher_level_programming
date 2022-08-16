66bbd006ba3eeaa8af96ddb5498c9682e7996c6b
0806socrrn <socrrn@gmail.com>
2022-08-16 16:28:51 -0500

    🚧 construction project

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,4 +1,12 @@
 #!/usr/bin/node
-/*Write a function that returns the reversed version of a list:
+/* Write a function that returns the reversed version of a list:
 Prototype: exports.esrever = function (list)
-You are not allow to use the built-in method reverse*/
+You are not allow to use the built-in method reverse */
+exports.esrever = function (list) {
+  const reversedArray = [];
+
+  for (const rev in list) {
+    reversedArray[rev] = list[list.length - rev - 1];
+  }
+  return (reversedArray);
+};
