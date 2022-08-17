cb9156c158c34a0ef6a563c632019685d297be1d
0806socrrn <socrrn@gmail.com>
2022-08-17 16:27:31 -0500

    🚧 construction project

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,20 @@
 #!/usr/bin/node
-/*Write a script that writes a string to a file.
+/* Write a script that writes a string to a file.
 The first argument is the file path
 The second argument is the string to write
 The content of the file must be written in utf-8
-If an error occurred during while writing, print the error object*/
+If an error occurred during while writing, print the error object */
+const { argv } = require('process');
+const { writeFile } = require('fs');
+const filename = argv[2];
+const content = argv[3];
+
+try {
+  writeFile(filename, content, err => {
+    if (err) {
+      console.log(err);
+    }
+  });
+} catch (err) {
+  console.log(err);
+}
