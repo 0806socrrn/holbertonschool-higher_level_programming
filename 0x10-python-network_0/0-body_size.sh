c42855089a2e8ee35549985fa9f75e20be81d611
= <=>
2022-09-02 14:29:57 -0500

    construct

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#script that takes in a URL, sends a request to that URL,
+#and displays the size of the body of the responseThe size must be displayed in bytes
+#You have to use curl
+curl -sI "$1" | grep 'Content-Length:' | cut -c 17-
